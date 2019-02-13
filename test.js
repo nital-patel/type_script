805fbec7fecde8ff55a117900665042ef12fe32c
Nital Patel <patelnital36@gmail.com>
2019-02-13 12:47:01 -0500

    Add void method

--- test.js
+++ test.js
@@ -2,3 +2,13 @@ var message = "Hello World";
 console.log(message);
 var num = 12;
 console.log(num);
+var Greeting = /** @class */ (function () {
+    function Greeting() {
+    }
+    Greeting.prototype.greet = function () {
+        console.log("Hello World!!");
+    };
+    return Greeting;
+}());
+var obj = new Greeting();
+obj.greet();
--- test.ts
+++ test.ts
@@ -1,5 +1,14 @@
 var message:string = "Hello World";
 console.log(message);
 
+
 var num:number = 12;
 console.log(num);
+
+class Greeting {
+    greet():void {
+        console.log("Hello World!!");
+    }
+}
+var obj = new Greeting();
+obj.greet();

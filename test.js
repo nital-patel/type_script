758be4ec47fa21f1f31e7dbdb5fb374dce7eb0c2
Nital Patel <patelnital36@gmail.com>
2019-02-13 14:38:59 -0500

    Add variable

--- test.js
+++ test.js
@@ -12,3 +12,11 @@ var Greeting = /** @class */ (function () {
 }());
 var obj = new Greeting();
 obj.greet();
+var message = "John";
+var score1 = 50;
+var score2 = 42.50;
+var sum = score1 + score2;
+console.log("message:" + message);
+console.log("first score: " + score1);
+console.log("second score: " + score2);
+console.log("sum of the scores " + sum);
--- test.ts
+++ test.ts
@@ -12,3 +12,13 @@ class Greeting {
 }
 var obj = new Greeting();
 obj.greet();
+
+var message:string = "John";
+var score1:number = 50;
+var score2:number = 42.50
+var sum = score1 + score2
+console.log("message:"+ message)
+console.log("first score: "+score1)
+console.log("second score: "+score2)
+console.log("sum of the scores "+sum)
+

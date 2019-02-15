b16eb50115d00578b6a3b21eb004ca9fe5fb1a3b
Nital Patel <patelnital36@gmail.com>
2019-02-15 14:08:56 -0500

    for loop

--- test.js
+++ test.js
@@ -67,3 +67,22 @@ while (n > 5) {
 do {
     console.log("Entered do_while");
 } while (n > 5);
+//break Statement
+var i = 1;
+while (i <= 10) {
+    if (i % 5 == 0) {
+        console.log("The first multiple of 5 between 1 and 10 is : " + i);
+        break; //exit the loop if the first multiple is fond
+    }
+    i++;
+} //ouputs 5 and exits the loop
+//continue Statement
+var num = 0;
+var count = 0;
+for (num = 0; num <= 20; num++) {
+    if (num % 2 == 0) {
+        continue;
+    }
+    count++;
+}
+console.log("The count of odd values between 0 and 20 is: " + count);
--- test.ts
+++ test.ts
@@ -75,3 +75,26 @@ do {
     console.log("Entered do_while")
 }
 while(n>5)
+
+
+//break Statement
+var i:number = 1
+while(i<=10) {
+    if (i%5 == 0) {
+        console.log("The first multiple of 5 between 1 and 10 is : "+i)
+        break                //exit the loop if the first multiple is fond
+    }
+i++
+}                             //ouputs 5 and exits the loop
+
+//continue Statement
+var num:number = 0
+var count:number = 0;
+
+for(num=0;num<=20;num++) {
+    if (num % 2==0) {
+        continue
+    }
+    count ++
+}
+console.log("The count of odd values between 0 and 20 is: "+count)
